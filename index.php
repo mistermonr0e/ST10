0a530e198dba04058345f793069c320494774205
BuildTools <unconfigured@null.spigotmc.org>
2017-06-02 22:59:03 -0400

    L.O.A Setup
    added rough html and css for LOA system

--- index.php
+++ index.php
@@ -52,7 +52,20 @@ echo '
 echo '
 	<div id="row3" class="row"> 
 		<h3 class="text-center">L.O.A</h3>
-		
+		<form>
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
 	</div>
 ';
 //close all of main body
