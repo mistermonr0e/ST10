92118abd8f0c6ddbcf6926040f8b9c69ba7a7a93
BuildTools <unconfigured@null.spigotmc.org>
2017-06-03 15:22:25 -0400

    Sql
    sql for loa is half done

--- index.php
+++ index.php
@@ -58,13 +58,10 @@ echo '
     <input type="email" class="form-control" id="email">
   </div>
   <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd">
+    <label for="Reason">Reason:</label>
+    <input type="text" class="form-control" id="pwd">
   </div>
-  <div class="checkbox">
-    <label><input type="checkbox"> Remember me</label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
+  <button id="submit" type="submit" class="btn btn-default">Submit</button>
 </form>
 	</div>
 ';
